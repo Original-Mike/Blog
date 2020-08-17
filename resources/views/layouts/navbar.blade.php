aa481ecb5e078a8c00d38b187e82db5d5197bc8a
Mike <mikerahawarin@gmail.com>
2020-08-17 22:51:16 +0200

    Updated navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,11 @@
-<div class="bg-main">
-    <div class="container mx-auto">
+<div class="bg-teal-600">
+    <div class="container mx-auto flex justify-between items-center py-2">
         <div>
             <img src="https://original-mike.com/images/logo.png" alt="Logo">
         </div>
         <div>
-            <a href="#">Berichten</a>
-            <a href="#">Contact</a>
+            <a href="#" class="inline-block p-2 text-teal-100 hover:text-teal-200">Berichten</a>
+            <a href="#" class="inline-block p-2 text-teal-100 hover:text-teal-200">Contact</a>
         </div>
     </div>
 </div>
