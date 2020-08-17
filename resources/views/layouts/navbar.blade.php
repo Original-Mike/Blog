0592123e04a89ff0df856c92acaeb93d9b769eea
Mike <mikerahawarin@gmail.com>
2020-08-17 22:27:46 +0200

    Added beginning of index and navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-main">
+    <div class="container mx-auto">
+        <div>
+            <img src="https://original-mike.com/images/logo.png" alt="Logo">
+        </div>
+        <div>
+            <a href="#">Berichten</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>
+</div>
