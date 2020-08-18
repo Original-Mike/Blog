15442f81c75cc708f413620af64e11318563aca8
Mike <mikerahawarin@gmail.com>
2020-08-18 10:49:05 +0200

    Added Header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,10 @@
+<header class="w-full container mx-auto">
+    <div class="flex flex-col items-center py-12">
+        <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="{{ url('https://www.original-mike.com') }}">
+            ORIGINAL-MIKE
+        </a>
+        <p class="text-lg text-gray-600">
+            Everything well done, except my Steak <i class="fal fa-smile-wink"></i>
+        </p>
+    </div>
+</header>
