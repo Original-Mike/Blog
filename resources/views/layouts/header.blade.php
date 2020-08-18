4fbffdeadd2275f4d51b171862b50346753d892d
Mike <mikerahawarin@gmail.com>
2020-08-18 11:09:07 +0200

    Updated header and index

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,10 @@
 <header class="w-full container mx-auto">
     <div class="flex flex-col items-center py-12">
-        <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="{{ url('https://www.original-mike.com') }}">
+        <a class="font-bold text-gray-900 uppercase text-5xl" href="{{ url('https://www.original-mike.com') }}">
             ORIGINAL-MIKE
         </a>
         <p class="text-lg text-gray-600">
+            <!-- Special thanks to Harvey Chow from http://hcdesigns.nl/ for the following text -->
             Everything well done, except my Steak <i class="fal fa-smile-wink"></i>
         </p>
     </div>
