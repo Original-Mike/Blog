9a31d94fce90093e58c7b76da4e7b600fef05869
Mike <mikerahawarin@gmail.com>
2020-08-19 12:16:49 +0200

    Updated route and indexcontroller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+        return view('index', compact('posts'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/', 'HomeController@index');
+Route::get('/', 'IndexController@index');
 
 Auth::routes();
 
