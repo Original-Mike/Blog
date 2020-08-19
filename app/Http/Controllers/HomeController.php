dce66f1a48170e19f18bc93377d0f6b662dfa6e9
Mike <mikerahawarin@gmail.com>
2020-08-19 11:57:21 +0200

    Updated HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -25,12 +24,6 @@ class HomeController extends Controller
     public function home()
     {
         return view('home');
-    }
-
-    public function index()
-    {
-        $posts = Post::orderBy('created_at', 'DESC')->get();
-        return view('index', compact('posts'));
     }
 
 }
