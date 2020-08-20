40622239d7f2083b7be769f96c558cedaa566e94
Mike <mikerahawarin@gmail.com>
2020-08-20 10:48:09 +0200

    Updated main app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Blog | Original-Mike @yield('name')</title>
+    <title>Original-Mike | Blog @yield('name')</title>
 
     <link href="#" rel="icon">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
